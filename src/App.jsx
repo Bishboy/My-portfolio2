f6541cde5a6b98e9702572a12b006920683d0174
Bishboy <Bishboy2spieking@gmail.com>
2024-11-05 17:01:09 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Sparkles } from '@react-three/drei'
+import Navbar from './sections/Navbar'
+
+// const RotatingCube = () => {
+//   const meshRef = useRef();
+
+//   useFrame(()=>{
+//     if(meshRef.current){
+//       meshRef.current.rotation.y += 0.001
+//        meshRef.current.rotation.x += 0.001;
+//     }
+//   })
+//   return(
+//     <mesh ref={meshRef}>
+//       <cylinderGeometry args={[1,1,1 ]} /> 
+//       <meshLambertMaterial color="#468585" emissive="#468585" />
+//       <Sparkles count={100} scale={1} size={6} speed={0.002} noise={0.2} />
+
+//     </mesh>
+//   )
+// }
+
+function App() {
+  return (
+    <main className=''>
+      <Navbar />
+    </main>
+    // <Canvas style={{height:"100vh"}} className=' text-black w-screen  flex justify-center items-center'>
+    //   <OrbitControls enablePan enableRotate  enableZoom />
+    //   <directionalLight position={[1,1,1]} intensity={10} color={0x9cdba6} /> 
+    //   <color attach="background" args={['#F0F0F0']} />
+    //   <RotatingCube />
+    // </Canvas>
+  )
+}
+
+export default App
